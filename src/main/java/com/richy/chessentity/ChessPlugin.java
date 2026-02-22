96344c0ccf9277331febcb46f073810e979f6c0c
Richy <103146442+Littlytale@users.noreply.github.com>
2026-02-22 15:16:52 +0100

    Setup Plugin/Gradle

---
+++ src/main/java/com/richy/chessentity/ChessPlugin.java
@@ -0,0 +1,26 @@
+package com.richy.chessentity;
+
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.Interaction;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import java.util.logging.Level;
+
+import javax.annotation.Nonnull;
+
+
+public class ChessPlugin extends JavaPlugin {
+
+    public ChessPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        getLogger().at(Level.INFO).log("ExamplePlugin setup...");
+    }
+
+    @Override
+    protected void shutdown() {
+        getLogger().at(Level.INFO).log("Shutdown ExamplePlugin...");
+    }
+}
